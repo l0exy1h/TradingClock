1a07b2008e889eac3b6cd91c4742cb7ab420242d
l0exy1h <y0rkl1u@outlook.com>
2025-01-05 22:47:15 -0800

    v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { Container, Stack } from '@mui/material';
+import { Box, Container, Stack } from '@mui/material';
 import Config from './Config';
 import Clock from './Clock';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
   return (
-    <Container sx={{ width: '100vw' }}>
-      <Stack spacing={2}>
-        <Config />
-        <Clock />
-      </Stack>
-    </Container>
+    <ThemeProvider theme={lightTheme}> 
+      <Box sx={{backgroundColor:"#FFFFFF", height: '100vh'}}>
+        <Container sx={{ width: '100vw' }}>
+          <Stack spacing={2}>
+            <Config />
+            {/* <Clock /> */}
+          </Stack>
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
 
